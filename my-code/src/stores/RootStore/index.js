e8d1406315f40ac593f8388dedd81652fefb7369
Eduardo Santos <e3duardo@live.com>
2018-10-22 08:28:52 -0300

    removing files from branch master

--- my-code/src/stores/RootStore/index.js
+++
@@ -1,9 +0,0 @@
-import UiStore from '../UiStore'
-import MovieStore from '../MovieStore'
-
-export default class RootStore {
-  constructor() {
-    this.uiStore = new UiStore(this);
-    this.movieStore = new MovieStore(this);
-  }
-}
